1e6020745cc77858f0c42ce397bf328bb1e0b0de
Rick <rick@fbag.dev>
2026-02-20 13:00:57 +0100

    Mirror debug output to Serial and stdout in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,44 @@
 #include "cst816.h"
 #include "lcd_bl_pwm_bsp.h"
 #include "lcd_config.h"
+#include <stdio.h>
+
+#define DBG_BOTH(fmt, ...)                        \
+  do {                                            \
+    Serial.printf(fmt, ##__VA_ARGS__);            \
+    Serial.flush();                               \
+    printf(fmt, ##__VA_ARGS__);                   \
+    fflush(stdout);                               \
+  } while (0)
+
 void setup()
 {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
+  setvbuf(stdout, NULL, _IONBF, 0);
 
-  // Give USB CDC enough time and emit repeated markers so monitor can catch them.
-  uint32_t start_ms = millis();
-  while (!Serial && (millis() - start_ms) < 8000) {
-    delay(10);
-  }
+  // Keep startup deterministic and always emit markers even if CDC attach is late.
+  delay(100);
 
   for (int i = 0; i < 20; i++) {
-    Serial.printf("[DBG] boot banner %d/20, millis=%lu\r\n", i + 1, (unsigned long)millis());
-    Serial.flush();
+    DBG_BOTH("[DBG] boot banner %d/20, millis=%lu\r\n", i + 1, (unsigned long)millis());
     delay(200);
   }
 
-  Serial.println("[DBG] setup: start");
+  DBG_BOTH("[DBG] setup: start\r\n");
   Touch_Init();
-  Serial.println("[DBG] setup: touch init done");
+  DBG_BOTH("[DBG] setup: touch init done\r\n");
   lcd_bl_pwm_bsp_init(LCD_PWM_MODE_255);
-  Serial.println("[DBG] setup: backlight init done");
+  DBG_BOTH("[DBG] setup: backlight init done\r\n");
   lcd_lvgl_Init();
-  Serial.println("[DBG] setup: lcd_lvgl_Init returned");
+  DBG_BOTH("[DBG] setup: lcd_lvgl_Init returned\r\n");
 }
 void loop()
 {
   static uint32_t last_heartbeat = 0;
   if (millis() - last_heartbeat > 1000) {
     last_heartbeat = millis();
-    Serial.printf("[DBG] heartbeat millis=%lu\r\n", (unsigned long)last_heartbeat);
+    DBG_BOTH("[DBG] heartbeat millis=%lu\r\n", (unsigned long)last_heartbeat);
   }
 #ifdef Backlight_Testing
   setUpdutySubdivide(LCD_PWM_MODE_255);
